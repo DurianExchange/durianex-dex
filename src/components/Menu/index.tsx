31e14478499e2dc7b45814224b182465a6d8a020
DurianExchange <musang.king@durian.exchange>
2021-05-15 09:53:29 +0800

    Remove multi language support

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,9 +1,11 @@
 import React, { useContext } from 'react'
 import { Menu as UikitMenu, ConnectorId } from '@durianexchange/uikit'
 import { useWeb3React } from '@web3-react/core'
+import { allLanguages } from 'constants/localisation/languageCodes'
 import { LanguageContext } from 'hooks/LanguageContext'
 import useTheme from 'hooks/useTheme'
 import useGetPriceData from 'hooks/useGetPriceData'
+import useGetLocalProfile from 'hooks/useGetLocalProfile'
 import { injected, bsc, walletconnect } from 'connectors'
 import links from './config'
 
@@ -13,6 +15,7 @@ const Menu: React.FC = (props) => {
   const { isDark, toggleTheme } = useTheme()
   const priceData = useGetPriceData()
   const cakePriceUsd = priceData ? Number(priceData.prices.Cake) : undefined
+  const profile = useGetLocalProfile()
 
   return (
     <UikitMenu
